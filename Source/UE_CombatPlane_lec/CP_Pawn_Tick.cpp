81a56d72d495e62d6b5ef99031dbfdb5a1aef3ce
HoonInPark <hooninpark@gmail.com>
2023-09-04 19:53:58 +0900

    making Curve

--- Source/UE_CombatPlane_lec/CP_Pawn_Tick.cpp
+++ Source/UE_CombatPlane_lec/CP_Pawn_Tick.cpp
@@ -7,39 +7,19 @@ ACP_Pawn_Tick::ACP_Pawn_Tick()
 {
 }
 
-void ACP_Pawn_Tick::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-	
-	MoveForwardWithCurve(DeltaTime);
-}
-
 void ACP_Pawn_Tick::BeginPlay()
 {
 	Super::BeginPlay();
+
 }
 
-void ACP_Pawn_Tick::MoveForward(float _DeltaTime)
+void ACP_Pawn_Tick::Tick(float DeltaTime)
 {
-	SetActorLocation(GetActorLocation() + FVector(_DeltaTime * 1000.f, 0.f, 0.f));
-}
+	Super::Tick(DeltaTime);
 
-void ACP_Pawn_Tick::MoveForwardAfterTakeOff(float _DeltaTime)
-{
-	if (GetActorLocation().Z < 1000.f)
-		SetActorLocation(GetActorLocation() + FVector(0.f, 0.f, _DeltaTime * 500.f));
-	else
-		SetActorLocation(GetActorLocation() + FVector(_DeltaTime * 1000.f, 0.f, 0.f));
-}
+	//CPLOG(Warning, TEXT(" DelataTime : %f"), DeltaTime);
 
-void ACP_Pawn_Tick::MoveForwardWhileTakeOff(float _DeltaTime)
-{
-	SetActorLocation(GetActorLocation() + FVector(_DeltaTime * 100.f, 0.f, _DeltaTime *_DeltaTime * 750.f));
-}
+	SetActorLocation(GetActorLocation() + FVector(DeltaTime * 500.f, 0.f, DeltaTime * DeltaTime * 5000.f));
+	SetActorRotation(GetActorRotation() + FRotator(DeltaTime * 10.f, 0.f, 0.f));
 
-void ACP_Pawn_Tick::MoveForwardWithCurve(float _DeltaTime)
-{
-	SetActorLocation(GetActorLocation() + FVector(_DeltaTime * 100.f, 0.f, _DeltaTime *_DeltaTime * 750.f));
-	SetActorRotation(GetActorRotation() + FRotator(_DeltaTime * 5.f, 0.f, 0.f));
 }
-
--- Source/UE_CombatPlane_lec/CP_Pawn_Tick.h
+++ Source/UE_CombatPlane_lec/CP_Pawn_Tick.h
@@ -3,29 +3,25 @@
 #pragma once
 
 #include "UE_CombatPlane_lec.h"
-#include "CP_Pawn_CombatPlane.h"
+#include "CP_Pawn_Construct.h"
 #include "CP_Pawn_Tick.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class UE_COMBATPLANE_LEC_API ACP_Pawn_Tick : public ACP_Pawn_CombatPlane
+class UE_COMBATPLANE_LEC_API ACP_Pawn_Tick : public ACP_Pawn_Construct
 {
 	GENERATED_BODY()
 	
 public:
 	ACP_Pawn_Tick();
 
-private:
-	virtual void Tick(float DeltaTime) override;
-
 protected:
+	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:
-	void MoveForward(float _DeltaTime);
-	void MoveForwardAfterTakeOff(float _DeltaTime);
-	void MoveForwardWhileTakeOff(float _DeltaTime);
-	void MoveForwardWithCurve(float _DeltaTime);
+public:
+	virtual void Tick(float DeltaTime) override;
+
 };
